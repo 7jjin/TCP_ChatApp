df1c7975254c13614f2f7353d940928caa5411ab
진형 조 <wlsgud6305@naver.com>
2024-11-29 10:18:47 +0900

    초기 커밋.

---
+++ TCP_ChatApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TCP_ChatApp.Forms;
+
+namespace TCP_ChatApp
+{
+
+    internal static class Program
+    {
+        private static FormMain formMain;
+
+        /// <summary>
+        /// 해당 애플리케이션의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(formMain = new FormMain());
+        }
+    }
+}
